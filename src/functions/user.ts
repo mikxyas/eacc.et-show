8b51be7a4d68ab991989463ca05b87242be07d5f
Mikiyas <mymikiaa11@gmail.com>
2024-06-18 20:02:26 +0300

    vercel build error

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,6 +1,8 @@
-import { supabase } from "@/libs/supabase";
+// import { supabase } from "@/libs/supabase";
+import { createClient } from "@/utils/supabase/client";
 
 export async function initUser() {
+    const supabase = createClient()
     const sesh = await supabase.auth.getSession()
     if (sesh) {
         return sesh.data
